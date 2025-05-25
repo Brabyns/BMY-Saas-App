d96ed3784ee2a332e97748faec973d606c1481bf
Brabyns Yabwetsa <burabuyabz@gmail.com>
2025-05-25 21:27:44 +0300

    Implement Clerk Auth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
 import NavItems from "@/components/NavItems";
-
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 const Navbar = () => {
     return (
@@ -11,11 +16,20 @@ const Navbar = () => {
                     <Image src="/images/logo.svg" alt="Logo" width={46} height={44} />
                 </div>
             </Link>
-            <div className="flex items-center gap-8 cursor-pointer">
+            <div className="flex items-center gap-8">
                 <NavItems/>
-                <p>Sign In</p>
-            </div>
+                <div className="flex items-center gap-4">
+                    <SignedOut>
+                        <SignInButton>
+                            <button className="btn-signin">Sign In</button>
+                        </SignInButton>
 
+                    </SignedOut>
+                    <SignedIn>
+                        <UserButton/>
+                    </SignedIn>
+                </div>
+            </div>
         </nav>
     )
 }
