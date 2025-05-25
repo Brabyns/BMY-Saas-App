84f0a1c3b1c43a121338f7dc4281b1ab8d9cbb46
Brabyns Yabwetsa <burabuyabz@gmail.com>
2025-05-25 15:11:43 +0300

    Implement Home Page UI Design

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,53 @@
 import React from 'react'
-import {Button} from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import Cta from "@/components/CTA";
+import {recentSessions} from "@/constants";
+
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to my Saas App</h1>
-        <Button>Click Me</Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+        <section className="home-section">
+            <CompanionCard
+                id="123"
+                name="Neura the Brainy Explorer"
+                topic="Neural Network of the Brain"
+                subject="Sceince"
+                duration={45}
+                color="#ffdae6"
+            />
+            <CompanionCard
+                id="456"
+                name="Countsy the Number Wizard"
+                topic="Derivatives & Integrals"
+                subject="Maths"
+                duration={30}
+                color="#e5d0ff"
+            />
+            <CompanionCard
+                id="478"
+                name="The Vocabulary Builder"
+                topic="English Literature"
+                subject="Language"
+                duration={50}
+                color="#3B83D0"
+            />
+
+        </section>
+
+        <section className="home-section">
+            <CompanionsList
+                title="Recently Completed Sessions"
+                companions={recentSessions}
+                classNames="2/3 max-lg:w-full"
+
+            />
+            <Cta />
+        </section>
+
+    </main>
   )
 }
 
